255c1d4d7f3cfe858c107aa4c23413e4220dd3f8
P A Chathura Madhava <chathuratechdev@gmail.com>
2024-05-21 10:56:52 +0530

    Initial commit

---
+++ src/authentication/jwt-auth.guard.ts
@@ -0,0 +1,11 @@
+// src/authentication/jwt-auth.guard.ts
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  getRequest(context: ExecutionContext) {
+    const ctx = context.switchToHttp().getRequest();
+    return ctx;
+  }
+}
